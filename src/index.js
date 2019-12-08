28a6b20a61ab1bf765e7212b175afd0b255904c4
S-Masakatsu <s.masakatsu197@gmail.com>
2019-12-08 20:27:39 +0900

    [add]Start-up webRTC

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@
 
 import 'reset_style.css'
 import 'style.css'
+import 'core-js'
+import 'regenerator-runtime/runtime'
 import {Signaling} from 'signaling'
 
-const webSock = new Signaling('ws://192.168.0.5:8080/');
+const webSock = new Signaling('ws://192.168.0.6:8080/');
+
+const startVideo = async() => {
+    try {
+        await navigator.mediaDevices.getUserMedia({video: true, audio: false});
+    } catch(err) {
+        console.error('mediaDevices.getUserMedia() error:', err);
+    }
+}
+
+(()=> {
+    startVideo();
+})();
