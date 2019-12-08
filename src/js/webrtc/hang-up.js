ad47c419f663a09fd718bb68ee28ccd2f50b4cf3
S-Masakatsu <s.masakatsu197@gmail.com>
2019-12-08 20:28:46 +0900

    [add]Disconnect P2P

---
+++ src/js/webrtc/hang-up.js
@@ -0,0 +1,28 @@
+'use strict'
+
+import {remoteVideo, peerConnection, negotiationneededCounter} from 'webrtc/prepare-connection'
+import {wsSock} from 'signaling'
+
+//--------------------------------------------------------------------------
+// P2Pの切断処理を行う
+//--------------------------------------------------------------------------
+export const hangUp = () => {
+    if(peerConnection.peerObj) {
+        if(peerConnection.peerObj.iceConnectionState !== false) {
+            peerConnection.peerObj.close();
+            peerConnection.peerObj = null;
+            negotiationneededCounter.negoCount = 0;
+            const message = JSON.stringify({type: 'close'});
+            console.log('sending close message');
+            wsSock.sock.send(message);
+            cleanupVideoElement(remoteVideo);
+            return;
+        }
+    }
+    console.log('peerConnection is closed.')
+}
+
+const cleanupVideoElement = element => {
+    element.pause();
+    element.srcObject = null;
+}
