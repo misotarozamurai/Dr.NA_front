4cb179ae3c36d5909162f116da97e7746ee3b319
S-Masakatsu <s.masakatsu197@gmail.com>
2019-12-16 14:15:08 +0900

    [add]mediaDevices

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import 'reset_style.css'
 import 'style.css'
 import 'core-js'
 import 'regenerator-runtime/runtime'
-import {WsSock} from 'socket'
+import {startVideo} from 'devise'
+// import {WsSock} from 'socket'
 
-const webSock = new WsSock('ws://localhost:8080/');
+// const webSock = new WsSock('ws://localhost:8080/');
 
 (()=> {
-    // 
+    startVideo();
 })();
--- src/js/devise/index.js
+++ src/js/devise/index.js
@@ -1,1 +1,36 @@
 'use strict'
+
+//--------------------------------------------------------------------------
+// getUserMediaで、カメラを操作する
+//--------------------------------------------------------------------------
+
+const videoStyle = {
+    aspectRatio: {
+        exact: 1.7777777778
+    }, 
+    width: window.parent.screen.width,
+    high: window.parent.screen.height
+}
+
+
+// ユーザからのメディア使用許可を求めて、許可されればストリームが返される。
+// 渡されたストリームからメディアの再生を行う。
+export const startVideo = async() => {
+    try {
+        let local_stream = await navigator.mediaDevices.getUserMedia({video: videoStyle, audio: false});
+        playVideo(local_stream);
+    } catch(err) {
+        console.error('mediaDevices.getUserMedia() error:', err);
+    }
+}
+
+// メディアデータを取得し、メディアの再生を開始する
+const playVideo = async(stream) => {
+    const local_video = document.getElementById('local-video');
+    local_video.srcObject = stream;
+    try {
+        await local_video.play();
+    } catch(err) {
+        console.log('auto play error', err);
+    }
+}
