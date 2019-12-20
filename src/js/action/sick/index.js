6ad81acffb7200f3fea281861cebbbbfa3417441
S-Masakatsu <s.masakatsu197@gmail.com>
2019-12-20 11:48:50 +0900

    [add]get sickData Animetion

--- src/js/action/sick/index.js
+++ src/js/action/sick/index.js
@@ -1,1 +1,36 @@
 'use strict'
+
+import {createCircle, createFadeText, removeWrapperChild} from 'action/sick/circle'
+import {resultDisplay} from 'action/result'
+// import {DNAAnimation} from './three/DNAAnimation'
+//--------------------------------------------------------------------------
+// 病気の解析を受け取りアニメーションを再生する
+//--------------------------------------------------------------------------
+export const animationPlayback = datas => {
+    // const animation = await new DNAAnimation();
+    // if(animation) {
+    //     document.body.appendChild(animation.Dom);
+
+
+    //     // アニメーションを再生する
+    //     animation.animate();
+    // }
+
+
+    createFadeText();
+    const fade_text = document.querySelector('#text-fade');
+    fade_text.addEventListener("animationend", e => {
+        removeWrapperChild();
+        createCircle(datas);
+    });
+
+    // resultDisplay(data);
+
+    // createCircle();
+    // console.log(data);
+
+    // result display
+
+}
+
+
