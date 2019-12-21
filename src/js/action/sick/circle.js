085b2fdb5be52cd39c42602e368b70db6bd18b32
S-Masakatsu <s.masakatsu197@gmail.com>
2019-12-21 18:25:39 +0900

    [add]Progress bar animation

---
+++ src/js/action/sick/circle.js
@@ -0,0 +1,71 @@
+'use strict'
+
+import {createElement, wrapperStyleToggle, removeWrapperChild} from 'element'
+import {resultDisplay} from 'action/sick/result'
+
+const divWrapper = document.getElementById('wrapper');
+const classWrapper = ['circle_wrapper', 'wrapper_back'];
+
+//--------------------------------------------------------------------------
+// プログレスバー作成前のアニメーションを作成
+//--------------------------------------------------------------------------
+export const createFadeText = () => {
+    wrapperStyleToggle(classWrapper);
+    console.log(classWrapper)
+
+    const fade_text = createElement('p', true, ['text-fade']);
+    fade_text.textContent = 'DNA解析を開始します';
+
+    divWrapper.appendChild(fade_text);
+}
+
+//--------------------------------------------------------------------------
+// プログレスバーの作成
+//--------------------------------------------------------------------------
+// Create a progress bar
+let ary = null;
+export const createCircle = (datas) => {
+    ary = datas;
+    // Create an element
+    const circle = createElement('div', false, ['circle']);
+    const circle_inner = createElement('div', false, ['circle_inner']);
+    circle_inner.textContent = 'DNA解析中';
+    const cup = createElement('p', true, ['cup']);
+
+    // Create parent-child relationship
+    circle_inner.appendChild(cup);
+    circle.appendChild(circle_inner);
+    divWrapper.appendChild(circle);
+
+    startTimer();
+}
+
+//--------------------------------------------------------------------------
+// プログレスバーの数値カウントアップを行う
+//--------------------------------------------------------------------------
+const startTimer = () => {
+    let num = 0;        // initial
+    const tgt = 125;    // upper limit
+    const speed = 80;   // speed
+    const cup = document.getElementById('cup');
+
+    setInterval(() => {
+        if(num <= tgt) {
+            // Display up to 100%
+            if(num <= 100) {
+                cup.textContent = num + '%';
+                // Change style when you reach 100%
+                if(num === 100) cup.classList.add('cup_complete');
+            } 
+            num ++;
+            if(num === tgt) stopTimer();
+        }
+    }, speed);
+}
+
+const stopTimer = () => {
+    clearInterval(startTimer);
+    removeWrapperChild();
+    wrapperStyleToggle(classWrapper);
+    resultDisplay(ary);
+}
