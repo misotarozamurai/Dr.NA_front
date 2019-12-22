271440f1aaa3b1f559e88388a836d97032f9d277
S-Masakatsu <s.masakatsu197@gmail.com>
2019-12-22 20:29:09 +0900

    [add]start ECG

--- src/js/action/index.js
+++ src/js/action/index.js
@@ -1,12 +1,18 @@
 'use strict'
 
 import {animationPlayback} from 'action/sick'
+import {createPulse} from 'action/pulse'
 
 //--------------------------------------------------------------------------
 // データ受信をしたら制御を行う
 //--------------------------------------------------------------------------
 export const getResultData = datas => {
-    const sick = datas.sick;
+    // Create ECG from pulse data
+    const pulse = datas.pulse;
+    const height = datas.height;
+    createPulse(pulse, height);
+
     // Start animation from sick data
+    const sick = datas.sick;
     animationPlayback(sick);
 }
